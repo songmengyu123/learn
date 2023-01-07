db0dd75aa35a79005af2a76d2c26340972fab3a3
songmengyu123 <13916117561@163.com>
2023-01-07 23:26:52 +0800

    learn.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{height:800}}>
+      <div style={{height: 48,backgroundColor:"#5584ff"}}>
+      1
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div style={{height:"calc(100% - 48px)",backgroundColor:"blue"}}>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
